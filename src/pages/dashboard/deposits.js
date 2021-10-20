772bac877df3bd062ec7cfad328f6d6bd2837711
andrewscarlos <andrews.knct@gmail.com>
2021-10-20 00:43:19 -0300

    feat: added occuredAt to payment status

--- src/pages/dashboard/deposits.js
+++ src/pages/dashboard/deposits.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from '@material-ui/core/Link';
+import { format,moment, formatDistance, formatRelative, subDays } from 'date-fns'
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
@@ -14,6 +14,8 @@ const useStyles = makeStyles({
     },
 });
 
+const dataFormat =  format( new Date (), 'yyyy-MM-dd')
+
 export default function Deposits() {
     const classes = useStyles();
     return (
@@ -21,13 +23,9 @@ export default function Deposits() {
             <Typography component="p" variant="h4">
                 $3,024.00
             </Typography>
-            <Typography color="textSecondary" className={classes.depositContext}>
-                on 15 March, 2019
-            </Typography>
             <div>
-                <Link color="primary" href="#" onClick={preventDefault}>
-                    View balance
-                </Link>
+                <p> Atendidos Hoje </p>
+                {dataFormat}
             </div>
         </React.Fragment>
     );
